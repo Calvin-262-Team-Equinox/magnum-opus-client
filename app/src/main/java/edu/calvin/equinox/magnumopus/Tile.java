18d49d24eadc6609748e3c0c1458699cd2faa4f5
Alpha <ngcoder@live.com>
2016-09-25 12:39:58 -0400

    * Tile: Draft sychronization constructs

--- app/src/main/java/edu/calvin/equinox/magnumopus/Tile.java
+++ app/src/main/java/edu/calvin/equinox/magnumopus/Tile.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * A section of the canvas.
  */
@@ -27,6 +29,11 @@ public class Tile
     private Canvas m_drawLayerCanvas;
 
     /**
+     * Bitmap of edits that are in progress of synchronizing with the server.
+     */
+    private Bitmap m_syncingLayer;
+
+    /**
      * Verified synchronized tile bitmap from the server.
      */
     private Bitmap m_syncedLayer;
@@ -44,15 +51,35 @@ public class Tile
      */
     private Canvas m_compositeCanvas;
 
+    /**
+     * True if there are active edits on m_drawLayer.
+     */
+    private boolean m_isDirty;
+
+    // Valid states for m_syncState.
+    private static final int NOT_SYNCING     = 0;
+    private static final int SYNCING         = 1;
+    private static final int COMPLETING_SYNC = 2;
+
+    /**
+     * State lock to avoid multiple syncs occurring at the same time.
+     */
+    private AtomicInteger m_syncState;
+
     public Tile()
     {
         m_drawLayer = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888);
         m_drawLayerCanvas = new Canvas(m_drawLayer);
 
+        m_syncingLayer = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888);
+
         m_syncedLayer = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888);
 
         m_composite = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888);
         m_compositeCanvas = new Canvas(m_composite);
+
+        m_isDirty = false;
+        m_syncState = new AtomicInteger(NOT_SYNCING);
 
         // Sample painting.
         Paint brush = new Paint();
@@ -72,8 +99,52 @@ public class Tile
         m_compositeCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
 
         m_compositeCanvas.drawBitmap(m_syncedLayer, 0, 0, null);
+        m_compositeCanvas.drawBitmap(m_syncingLayer, 0, 0, null);
         m_compositeCanvas.drawBitmap(m_drawLayer, 0, 0, null);
 
         return m_composite;
+    }
+
+    /**
+     * Dispatch current edits to the server for synchronization.
+     */
+    private void beginSyncEdits()
+    {
+        if (m_syncState.compareAndSet(NOT_SYNCING, SYNCING))
+        {
+            if (m_isDirty)
+            {
+                m_syncingLayer = Bitmap.createBitmap(m_drawLayer);
+                m_drawLayerCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+                m_isDirty = false;
+
+                // Dispatch m_syncingLayer to server.
+            }
+            else
+            {
+                // Dispatch m_syncVersion to server.
+            }
+        }
+    }
+
+    /**
+     * Apply a synchronized image from the server to this tile.
+     *
+     * @param syncedImg
+     *  The updated image.
+     * @param version
+     *  Version code of this image.
+     */
+    private void completeSyncEdits(Bitmap syncedImg, int version)
+    {
+        if (m_syncState.compareAndSet(SYNCING, COMPLETING_SYNC))
+        {
+            m_syncedLayer = syncedImg;
+            m_syncVersion = version;
+            m_syncingLayer = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888);
+
+            m_syncState.set(NOT_SYNCING);
+        }
     }
 }
