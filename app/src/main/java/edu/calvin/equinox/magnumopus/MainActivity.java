2999731d6f81374534501ed098e8eea0f7b0df06
caj7 <caj7@students.calvin.edu>
2016-09-28 19:22:39 -0400

    Added Documentation to File

--- app/src/main/java/edu/calvin/equinox/magnumopus/MainActivity.java
+++ app/src/main/java/edu/calvin/equinox/magnumopus/MainActivity.java
@@ -3,9 +3,16 @@ package edu.calvin.equinox.magnumopus;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+* MainActivity()
+* This is the main "entry point" for the app
+*/
 public class MainActivity extends AppCompatActivity
 {
-
+/**
+* OnCreate()
+* This creates the main activity layout
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
