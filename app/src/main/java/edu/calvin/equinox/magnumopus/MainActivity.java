865ea8b0ef7e6519760b4acb98bccd5dc9744965
hannahl95 <hel7@students.calvin.edu>
2016-10-13 21:58:36 -0400

    Create a user interface to be able to select a brush

--- app/src/main/java/edu/calvin/equinox/magnumopus/MainActivity.java
+++ app/src/main/java/edu/calvin/equinox/magnumopus/MainActivity.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 /**
  * MainActivity()
  * This is the main "entry point" for the app
  */
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
+    private Spinner brushSpinner;
+    public String brushType;
+
     /**
      * OnCreate()
      * This creates the main activity layout
@@ -22,6 +28,37 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        brushSpinner = (Spinner) findViewById(R.id.brushSpinner);
+        brushType = (String) brushSpinner.getSelectedItem();
+        brushSpinner.setOnItemSelectedListener(this);
+    }
+
+    /**
+     * Set the canvas's brush to the specified brush type when an item is selected
+     *
+     * @param adapterView
+     * @param view
+     * @param i
+     * @param l
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+    {
+        brushType = (String) brushSpinner.getSelectedItem();
+        TilingCanvasView theCanvas = (TilingCanvasView)findViewById(R.id.view);
+        theCanvas.setBrush(brushType);
+
+    }
+
+    /**
+     * If nothing is selected, do nothing
+     * @param adapterView
+     */
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView)
+    {
+
     }
 
     @Override
