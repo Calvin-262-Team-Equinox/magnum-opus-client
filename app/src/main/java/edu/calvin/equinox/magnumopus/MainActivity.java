8662b3209869695e01943b33fcbbc75b19963015
JahnDavis27 <jdavisnba13@gmail.com>
2016-12-13 18:00:11 -0500

    Added search functionality to open a canvas upon searching

--- app/src/main/java/edu/calvin/equinox/magnumopus/MainActivity.java
+++ app/src/main/java/edu/calvin/equinox/magnumopus/MainActivity.java
@@ -2,13 +2,25 @@ package edu.calvin.equinox.magnumopus;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * MainActivity()
@@ -31,8 +43,94 @@ public class MainActivity extends AppCompatActivity
         if (Intent.ACTION_SEARCH.equals(intent.getAction()))
         {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            Toast.makeText(this, "Unimplemented: open canvas <" + query + ">", Toast.LENGTH_LONG).show();
+            new GetSearchCanvas(this).execute("http://cs262.cs.calvin.edu:8085/equinox/search/canvas/" + query);
         }
+    }
+
+    private class GetSearchCanvas extends AsyncTask<String, Void, JSONObject>
+    {
+        String theUrl ="";
+        MainActivity m_Search;
+
+        public GetSearchCanvas(MainActivity search)
+        {
+            m_Search = search;
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params)
+        {
+            theUrl = params[0];
+
+            URL apiURL;
+            try
+            {
+                apiURL = new URL(theUrl);
+            } catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+                return null;
+            }
+
+            JSONObject output = null;
+
+            try{
+                HttpURLConnection conn = (HttpURLConnection)apiURL.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setConnectTimeout(10000); // 10 sec
+                conn.setReadTimeout(10000);    // 10 sec
+                conn.setRequestProperty("Content-Type", "application/json; charset=utf8");
+
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK)
+                {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream())
+                    );
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        result.append(line);
+                    }
+
+                    try
+                    {
+                        output = new JSONObject(result.toString());
+                    } catch (JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    Log.e("GetSearchCanvas", "HTTP error " + conn.getResponseCode());
+                }
+                conn.disconnect();
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            return output;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject canvasData)
+        {
+            Intent intent = new Intent(m_Search, CanvasActivity.class);
+            try
+            {
+                intent.putExtra("EXTRA_CANVAS_ID", canvasData.getInt("ID"));
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            m_Search.startActivity(intent);
+        }
+
+
     }
 
     @Override
